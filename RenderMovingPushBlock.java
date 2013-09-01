c8c7aefa71702a9f2d3a78e3d47ca9f396c5c36c
GotoLink <gotolinkminecraft@gmail.com>
2013-09-02 00:07:07 +0200

    Tried changing rendering

--- RenderMovingPushBlock.java
+++ RenderMovingPushBlock.java
@@ -2,12 +2,11 @@ package assets.handheldpiston;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStairs;
-import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.client.resources.ResourceLocation;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.MathHelper;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 import org.lwjgl.opengl.GL11;
@@ -17,9 +16,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class RenderMovingPushBlock extends Render
 {
-	private RenderBlocks renderBlock = new RenderBlocks();
-
-    public RenderMovingPushBlock()
+	public RenderMovingPushBlock()
     {
         shadowSize = 0.5F;
     }
@@ -27,24 +24,31 @@ public class RenderMovingPushBlock extends Render
     public void doRenderMovingPushBlock(EntityMovingPushBlock entitymovingpushblock, double d, double d1, double d2)
     {
     	Block block = Block.blocksList[entitymovingpushblock.blockID];
-        int metaData = entitymovingpushblock.metadata;
-        World world = entitymovingpushblock.getWorld();
-        GL11.glPushMatrix();
-        GL11.glTranslatef((float)d, (float)d1, (float)d2);
-        this.func_110775_a(entitymovingpushblock);
-        //loadTexture("/terrain.png");
-        GL11.glDisable(GL11.GL_LIGHTING);
-        this.renderBlock.blockAccess = world;
-        if (block instanceof BlockStairs)
-        {
-            customRenderBlockStairs(block, MathHelper.floor_double(entitymovingpushblock.posX), MathHelper.floor_double(entitymovingpushblock.posY), MathHelper.floor_double(entitymovingpushblock.posZ), metaData);
-        }
-        else if (block!=null)
-        {
-            customRender(block, metaData, world, MathHelper.floor_double(entitymovingpushblock.posX), MathHelper.floor_double(entitymovingpushblock.posY), MathHelper.floor_double(entitymovingpushblock.posZ));
-        }
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glPopMatrix();
+    	if(block!=null)
+    	{
+	        int metaData = entitymovingpushblock.metadata;
+	        World world = entitymovingpushblock.getWorld();
+	        GL11.glPushMatrix();
+	        GL11.glTranslatef((float)d, (float)d1, (float)d2);
+	        //this.func_110775_a(entitymovingpushblock);
+	        //loadTexture("/terrain.png");
+	        GL11.glDisable(GL11.GL_LIGHTING);
+	        this.renderBlocks.blockAccess = world;
+	    
+	        if (block instanceof BlockStairs)
+	        {
+	        	renderBlocks.setOverrideBlockTexture(block.getIcon(0, metaData));
+	            customRenderBlockStairs(block, entitymovingpushblock.posX, entitymovingpushblock.posY, entitymovingpushblock.posZ, metaData);
+	            renderBlocks.clearOverrideBlockTexture();
+	        }
+	        else if (block!=null)
+	        {
+	            customRender(block, metaData, world, entitymovingpushblock.posX, entitymovingpushblock.posY, entitymovingpushblock.posZ);
+	        }
+	        
+	        GL11.glEnable(GL11.GL_LIGHTING);
+	        GL11.glPopMatrix();
+    	}
     }
     @Override
     public void doRender(Entity entity, double d, double d1, double d2, float f, float f1)
@@ -52,49 +56,43 @@ public class RenderMovingPushBlock extends Render
         doRenderMovingPushBlock((EntityMovingPushBlock)entity, d, d1, d2);
     }
 
-    public void customRender(Block block, int i, World world, int j, int k, int l)
+    public void customRender(Block block, int i, World world, double posX, double posY, double posZ)
     {
-    	renderBlock.setRenderBoundsFromBlock(block);
-    	renderBlock.renderBlockSandFalling(block, world, j, k, l, i);
+    	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+1,posY-(int)posY+1,posZ-(int)posZ+1);
+    	renderBlocks.renderBlockSandFalling(block, world, (int)posX, (int)posY, (int)posZ, i);
     }
 
-    public boolean customRenderBlockStairs(Block block, int i, int j, int k, int l)
+    public void customRenderBlockStairs(Block block, double posX, double posY, double posZ, int l)
     {
-        boolean flag = false;
         if (l == 0)
         {
-            block.setBlockBounds(0.0F, 0.0F, 0.0F, 0.5F, 0.5F, 1.0F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            block.setBlockBounds(0.5F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            flag = true;
+        	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+0.5,posY-(int)posY+0.5,posZ-(int)posZ+1);
+            renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
+            renderBlocks.setRenderBounds(posX-(int)posX+0.5,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+1,posY-(int)posY+1,posZ-(int)posZ+1);
+        	renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
         }
         else if (l == 1)
         {
-            block.setBlockBounds(0.0F, 0.0F, 0.0F, 0.5F, 1.0F, 1.0F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            block.setBlockBounds(0.5F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            flag = true;
+        	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+0.5,posY-(int)posY+1,posZ-(int)posZ+1);
+        	renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
+        	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+1,posY-(int)posY+0.5,posZ-(int)posZ+1);
+        	renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
         }
         else if (l == 2)
         {
-            block.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 0.5F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            block.setBlockBounds(0.0F, 0.0F, 0.5F, 1.0F, 1.0F, 1.0F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            flag = true;
+        	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+1,posY-(int)posY+0.5,posZ-(int)posZ+0.5);
+        	renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
+        	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ+0.5,posX-(int)posX+1,posY-(int)posY+1,posZ-(int)posZ+1);
+        	renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
         }
         else if (l == 3)
         {
-            block.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.5F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            block.setBlockBounds(0.0F, 0.0F, 0.5F, 1.0F, 0.5F, 1.0F);
-            renderBlock.renderStandardBlock(block, i, j, k);
-            flag = true;
+        	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+1,posY-(int)posY+1,posZ-(int)posZ+1);
+        	renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
+        	renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ+0.5,posX-(int)posX+1,posY-(int)posY+0.5,posZ-(int)posZ+1);
+        	renderBlocks.renderStandardBlock(block, (int)posX, (int)posY, (int)posZ);
         }
-        block.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
-        return flag;
+        renderBlocks.setRenderBounds(posX-(int)posX,posY-(int)posY,posZ-(int)posZ,posX-(int)posX+1,posY-(int)posY+1,posZ-(int)posZ+1);
     }
 
 	@Override
